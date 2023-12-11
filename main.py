92a933fc247228ce62bd25e1707393ff8bfdbc5f
ShravaniRoy <shravani.thirunagari@gmail.com>
2023-12-11 18:56:40 +0530

    created a couple of routes for getting all todos and adding a todo using a list and also used models for req objects

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
+from models import Todo
 
 app = FastAPI()
 
-
+#all the below code is plain python
+#data validation is in built with fastapi, at pydantic
+#fastAPI is typed Python
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+todos = []
+# Get all todos
+@app.get("/todos")
+async def get_todos():
+    return {"todos": todos}
+
+# Get single todo
+
+# Create a todo
+@app.post("/todos")
+async def create_todo(todo: Todo):
+    todos.append(todo)
+    return {"message": "todo item is added"}
+
+# Update a todo
+
+# Delete a todo
